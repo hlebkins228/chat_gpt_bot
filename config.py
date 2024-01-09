a2630f818e2bcbc82c1c01555e572cccab03063c
Kozin Mikhail <kmv22u923@student.bmstu.ru>
2024-01-09 15:57:44 +0300

    fix config.py

--- config.py
+++ config.py
@@ -4,9 +4,9 @@ OPENAI_TOKEN = "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"
 chat_gpt_model = "gpt-3.5-turbo"
 
 db_host = "127.0.0.1"
-db_user = "hlebik"
+db_user = "hleb_db"
 db_password = "4572"
-db_name = "bot_users"
+db_name = "hleb_bot_users"
 
 user_files_path = "./data/user_messages/"
 
